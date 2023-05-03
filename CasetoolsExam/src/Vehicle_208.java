ec5e8bcb224967b6fc85a79cf3d6b82ae8fc6e1f
admin123 <admin123@Merlyn>
2023-05-04 07:53:33 +1400

    2nd commit

--- CasetoolsExam/src/Vehicle_208.java
+++ CasetoolsExam/src/Vehicle_208.java
@@ -8,6 +8,11 @@ public class Vehicle_208 {
 		this.brand=brand;
 		this.speed=0;
 	}
+	int decrease_speed(int decr)
+	{
+		return this.speed=speed - decr;
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
