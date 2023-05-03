3ab2d63bf012b91c99106c8d1aa788e388d1c055
admin123 <admin123@Merlyn>
2023-05-04 07:32:02 +1400

    first commit

---
+++ CasetoolsExam/src/Vehicle_208.java
@@ -0,0 +1,17 @@
+
+public class Vehicle_208 {
+	String brand;
+	int speed;
+	
+	Vehicle_208(String brand)
+	{
+		this.brand=brand;
+		this.speed=0;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
